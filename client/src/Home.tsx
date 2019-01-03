54315c4619020ac0f06f7a4197609b2debf1eb0a
Michael Ferris <mike.ferris@hotmail.com>
2019-01-03 03:50:02 -0500

    Add Navigatino menu

---
+++ client/src/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {
+  Glyphicon,
+  Nav,
+  Navbar,
+  NavItem,
+} from 'react-bootstrap';
+
+export default class NavMenu extends React.Component {
+  public displayName = NavMenu.name
+
+  public render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ client/src/NavMenu.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+  Glyphicon,
+  Nav,
+  Navbar,
+  NavItem,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+//import './NavMenu.css';
+
+export default class NavMenu extends React.Component {
+  public displayName = NavMenu.name
+
+  public render() {
+    return (
+      <Navbar
+        inverse={true}
+        fixedTop={true}
+        fluid={true}
+        collapseOnSelect={true}
+      >
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to={'/'}>What Are We Eating</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to={'/'} exact={true}>
+              <NavItem>
+                <Glyphicon glyph='home' /> Home
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
