62e359fc171033acf5c5b588e3fc911183abde3a
Michael Ferris <mike.ferris@hotmail.com>
2019-01-04 16:44:47 -0500

    Fix npm run dev

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const path = require("path");
 const cp = require("child_process");
 
-cp.execSync("npm run build-server");
-
-require("./build/src");
+const tsc = path.join(path.dirname(require.resolve("typescript")), "tsc.js");
+const out = cp.spawnSync("node", [tsc, "-p",  "."], {
+  stdio: "inherit"
+});
+if (out.status === 0) {
+  require("./build/src");
+} else {
+  process.exit(-1);
+}
